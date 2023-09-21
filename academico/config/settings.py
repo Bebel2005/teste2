c7416e07a5aac0a6f33d54277041a60f752ece93
Bebel2005 <113204375+Bebel2005@users.noreply.github.com>
2023-09-21 10:15:05 -0300

    Add files via upload

--- academico/config/settings.py
+++ academico/config/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'loja',
+        'NAME': 'academico',
         'PASSWORD' :'123456',
         'HOST': 'localhost',
         'PORT': '5432',
