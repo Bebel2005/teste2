5c1215d0ddd021785859f3e5f78287e2fd2f485d
Bebel2005 <113204375+Bebel2005@users.noreply.github.com>
2023-09-21 10:07:16 -0300

    Add files via upload

---
+++ academico/app/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from app.models import *
+
+admin.site.register(Pessoa)
+admin.site.register(Ocupacao)
+admin.site.register(Instituicao)
+admin.site.register(Area)
+admin.site.register(Curso)
+admin.site.register(Semestre)
+admin.site.register(Disciplina)
+admin.site.register(Matricula)
+admin.site.register(Avaliacao)
+admin.site.register(Frequencia)
+admin.site.register(Turma)
+admin.site.register(Cidade)
+admin.site.register(Advertencia)
+admin.site.register(Disciplina_curso)
+admin.site.register(Tipo_avaliacao)
